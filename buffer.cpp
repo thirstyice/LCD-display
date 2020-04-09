128c34c4bebcd2bd40d2a6a352829ec83e986c0b
thirstyice <tauranwood@gmail.com>
2020-04-09 08:19:51 -0600

    Init of FrameBuffer interaction code

---
+++ buffer.cpp
@@ -0,0 +1,40 @@
+#include "buffer.h"
+
+
+
+FrameBuffer::FrameBuffer(const char *fileName) {
+  int fbDevice=0;
+  // Open the file for reading and writing
+  fbDevice = open(fileName, O_RDWR);
+  if (!fbDevice) {
+    throw("Error: cannot open framebuffer device %s\n", fileName);
+  }
+  printf("The framebuffer device %s was opened successfully.\n", fileName);
+
+  // Get variable screen information
+  if (ioctl(fbDevice, FBIOGET_VSCREENINFO, &fbVarInfo)) {
+    throw("Error reading variable information.\n");
+  }
+  // Get fixed screen information
+  if (ioctl(fbDevice, FBIOGET_FSCREENINFO, &fbFixInfo)) {
+    throw("Error reading fixed information.\n");
+  }
+
+  // map framebuffer to user memory
+  fbSize = fbFixInfo.smem_len;
+
+  fbPointer = (char*)mmap(0,fbSize,PROT_READ | PROT_WRITE,MAP_SHARED,fbDevice, 0);
+  if ((int)fbPointer == -1) {
+    throw("Failed to mmap.\n");
+  }
+  // cleanup
+  close(fbDevice);
+}
+FrameBuffer::~FrameBuffer(){
+  munmap(fbPointer, fbSize);
+}
+void FrameBuffer::draw(int startPixel, int endPixel, int data) {
+  void * start = fbPointer+(startPixel*fbVarInfo.bits_per_pixel/8);
+  size_t length = (endPixel-startPixel)*fbVarInfo.bits_per_pixel/8;
+  memset(start, data, length);
+}
---
+++ buffer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+class FrameBuffer {
+	public:
+		FrameBuffer(const char *fileName = "/dev/fb0"); //bufferId is X in /dev/fbX
+		~FrameBuffer();
+		void draw(int startPixel, int endPixel, int data);
+		int getXres() {return fbVarInfo.xres;};
+		int getYres() {return fbVarInfo.yres;};
+	private:
+		char *fbPointer = 0;
+		struct fb_var_screeninfo fbVarInfo;
+		struct fb_fix_screeninfo fbFixInfo;
+		long int fbSize = 0;
+};
