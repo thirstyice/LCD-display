40e26cf455a0602101e6290eb1f8570efa8b0ac1
thirstyice <touranwood@gmail.com>
2020-04-11 04:42:15 -0600

    Screen: Add checks for drawing small rectangles
    0-size rectangles and single pixels are now handled seperately. Will 
    also check if a pixel is outside the screen and not draw if so.

--- screen.cpp
+++ screen.cpp
@@ -4,25 +4,20 @@ using namespace LCDDisplay;
 
 Screen::Screen(FrameBuffer* initBuffer) {
 	buffer=initBuffer;
-	screenArray = new uint32_t*[buffer->getXres()];
-	for (int i = 0; i<buffer->getXres(); i++) {
-		screenArray[i] = new uint32_t[buffer->getYres()];
-	}
-	for (int x=0; x<buffer->getXres(); x++) {
-		for (int y=0; y<buffer->getYres(); y++) {
-				screenArray[x][y] = bgColor;
-		}
-	}
+
 	drawRect(0,0,buffer->getXres(), buffer->getYres(), bgColor);
 }
 Screen::~Screen() {
-	for (int i=0;i<buffer->getXres(); i++) {
-		delete[] screenArray[i];
-	}
-	delete[] screenArray;
+
 }
 void Screen::drawRect(int xLoc, int yLoc, int xSize, int ySize, uint32_t color) {
-	printf("Drawing at:%i,%i size:%i,%i res:%i,%i\n",xLoc,yLoc,xSize,ySize,buffer->getXres(),buffer->getYres() );
+	if (xSize==0||ySize==0) {
+		return;
+	}
+	if (xSize==1&&ySize==1) {
+		Screen::drawPixel(xLoc,yLoc,color);
+		return;
+	}
 	uint8_t blue = color;
 	uint8_t green = color >> 8;
 	uint8_t red = color >> 16;
@@ -31,12 +26,6 @@ void Screen::drawRect(int xLoc, int yLoc, int xSize, int ySize, uint32_t color) 
 	}
 	if (yLoc+ySize>buffer->getYres()) {
 		ySize=buffer->getYres()-yLoc;
-	}
-	//Update the screenArray
-	for (int y=yLoc; y<ySize; y++) {
-		for (int x=xLoc; x<xSize; x++) {
-			screenArray[x][y]=color;
-		}
 	}
 	if (xLoc==0&&xSize==buffer->getXres()) {
 		// We can draw in one command
@@ -50,11 +39,12 @@ void Screen::drawRect(int xLoc, int yLoc, int xSize, int ySize, uint32_t color) 
 	}
 }
 void Screen::drawPixel(int xLoc, int yLoc, uint32_t color) {
+	if (xLoc>buffer->getXres()||xLoc<0||yLoc>buffer->getYres()||yLoc<0) {
+		return;
+	}
 	uint8_t blue = color;
 	uint8_t green = color >> 8;
 	uint8_t red = color >> 16;
-	// Update the screen array
-	screenArray[xLoc][yLoc]=color;
 	int pixel = yLoc*buffer->getXres()+xLoc;
 	buffer->draw(pixel, pixel+1, red, green, blue);
 }
