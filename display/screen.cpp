16cf36290e2be87e6e34e40749068269036a6ef7
thirstyice <touranwood@gmail.com>
2020-04-19 20:22:17 -0600

    Combine buffer and screen
    For more library-like behaviour

--- display/screen.cpp
+++ display/screen.cpp
@@ -15,16 +15,99 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>
 //
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
 #include "screen.h"
 
 using namespace LCDDisplay;
 
-Screen::Screen(FrameBuffer* initBuffer):View(0,0,initBuffer->getXres(),initBuffer->getYres()) {
-	buffer=initBuffer;
+Screen::Screen(const char* buffer):View(0,0,fbVarInfo.xres,fbVarInfo.yres) {
+	int fbDevice=0;
+	// Open the file for reading and writing
+	fbDevice = open(buffer, O_RDWR);
+	if (!fbDevice) {
+		throw("Error: cannot open framebuffer device %s\n", buffer);
+	}
+	// Get variable screen information
+	if (ioctl(fbDevice, FBIOGET_VSCREENINFO, &fbVarInfo)) {
+		throw("Error reading variable information.\n");
+	}
+	// Get fixed screen information
+	if (ioctl(fbDevice, FBIOGET_FSCREENINFO, &fbFixInfo)) {
+		throw("Error reading fixed information.\n");
+	}
+	// map framebuffer to user memory
+	fbSize = fbFixInfo.smem_len;
+
+	fbPointer = (unsigned char*)mmap(0,fbSize,PROT_READ | PROT_WRITE,MAP_SHARED,fbDevice, 0);
+	if ((int)fbPointer == -1) {
+		throw("Failed to mmap.\n");
+	}
+	// cleanup
+	close(fbDevice);
+
+	viewWidth=fbVarInfo.xres;
+	viewHeight=fbVarInfo.yres;
+
 	draw();
 }
 Screen::~Screen() {
+	munmap(fbPointer, fbSize);
+}
+void Screen::bufferDraw(int startPixel, int endPixel, uint8_t red, uint8_t green, uint8_t blue) {
+	if (startPixel > endPixel) {
+		// Allow bufferDrawing reversed lines
+		int tmp = startPixel;
+		startPixel = endPixel;
+		endPixel = tmp;
+	}
+	uint8_t bytesPerPixel=fbVarInfo.bits_per_pixel/8;
+	if (fbVarInfo.bits_per_pixel%8 != 0) {
+		// int division defaults to floor, we want it to ceil
+		bytesPerPixel++;
+	}
+	unsigned char * start = fbPointer+(startPixel*bytesPerPixel);
+	size_t length = endPixel-startPixel;
+	switch (fbVarInfo.bits_per_pixel) { // More color pallettes to be implemented
+		case 8: {
+			uint8_t color = ((red/32)<<5) + ((green/32) <<2) + blue/64;
+			memset(start, color, length);
+			break;
 
+		} case 16: {
+		// 	uint8_t colors[2] = {((blue/4)<<2) + ((red/8)>>3), (green/8) + ((red/8)<<5)};
+		// 	for (int pixel=0;pixel<length;pixel++){
+		// 		for (uint8_t byte=0; byte<2;byte++) {
+		// 			memset(start+(pixel*bytesPerPixel)+byte, colors[byte], 1);
+		// 		}
+		// 	}
+		uint8_t color= (green/32) + ((blue/64) << 3) + ((red / 32) << 5);
+#ifdef DEBUG
+		printf("Drawing at:%i for:%i ending:%i\n",start,length*2,start+(length*2));
+#endif //DEBUG
+		memset(start, color, length*2);
+		break;
+		} case 32: {
+			uint8_t colors[4] = {red, red, green, blue};
+			for (int pixel=0;pixel<length;pixel++){
+				for (uint8_t byte=0; byte<4;byte++) {
+					uint8_t colorByte = colors[byte];
+					memset(start+(pixel*bytesPerPixel)+byte, colorByte, 1);
+				}
+			}
+			break;
+		}
+		default: {
+			printf("Unknown colorspace: %i", fbVarInfo.bits_per_pixel);
+			return;
+		}
+	}
 }
 void Screen::drawRect(int xLoc, int yLoc, int xSize, int ySize, uint32_t color) {
 	if (xLoc>=viewWidth||yLoc>=viewHeight||(xLoc+xSize)<0||(yLoc+ySize)<0) {
@@ -55,24 +138,24 @@ void Screen::drawRect(int xLoc, int yLoc, int xSize, int ySize, uint32_t color) 
 	uint8_t blue = color;
 	uint8_t green = color >> 8;
 	uint8_t red = color >> 16;
-	if (xLoc==0&&xSize==buffer->getXres()) {
+	if (xLoc==0&&xSize==viewWidth) {
 		// We can draw in one command
-		int start = yLoc*buffer->getXres()+xLoc;
-		int end = (yLoc+ySize)*buffer->getXres();
-		buffer->draw(start, end, red, green, blue);
+		int start = yLoc*viewWidth+xLoc;
+		int end = (yLoc+ySize)*viewWidth;
+		bufferDraw(start, end, red, green, blue);
 	} else {
 		for (int y=0;y<ySize;y++) {
-			buffer->draw((yLoc+y)*buffer->getXres()+xLoc,(yLoc+y)*buffer->getXres()+xLoc+xSize, red, green, blue);
+			bufferDraw((yLoc+y)*viewWidth+xLoc,(yLoc+y)*viewWidth+xLoc+xSize, red, green, blue);
 		}
 	}
 }
 void Screen::drawPixel(int xLoc, int yLoc, uint32_t color) {
-	if (xLoc>buffer->getXres()||xLoc<0||yLoc>buffer->getYres()||yLoc<0) {
+	if (xLoc>viewWidth||xLoc<0||yLoc>viewHeight||yLoc<0) {
 		return;
 	}
 	uint8_t blue = color;
 	uint8_t green = color >> 8;
 	uint8_t red = color >> 16;
-	int pixel = yLoc*buffer->getXres()+xLoc;
-	buffer->draw(pixel, pixel+1, red, green, blue);
+	int pixel = yLoc*viewWidth+xLoc;
+	bufferDraw(pixel, pixel+1, red, green, blue);
 }
--- display/screen.h
+++ display/screen.h
@@ -17,19 +17,23 @@
 
 #pragma once
 
-#include <cstdint>
-
-#include "buffer.h"
 #include "view.h"
+
+#include <linux/fb.h>
+#include <cstdint>
 
 namespace LCDDisplay {
 	class Screen: public View {
 	public:
-		Screen(FrameBuffer* buffer);
+		Screen(const char* buffer = "/dev/fb1");
 		~Screen();
 		void drawRect(int xLoc, int yLoc, int xSize, int ySize, uint32_t color) override;
 		void drawPixel(int xLoc, int yLoc, uint32_t color) override;
 	private:
-		FrameBuffer* buffer;
+		void bufferDraw(int startPixel, int endPixel, uint8_t red, uint8_t green, uint8_t blue);
+		unsigned char *fbPointer = 0;
+		struct fb_var_screeninfo fbVarInfo;
+		struct fb_fix_screeninfo fbFixInfo;
+		long int fbSize = 0;
 	};
 }
